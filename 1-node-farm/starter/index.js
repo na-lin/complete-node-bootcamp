9abaa2b956a0e397409690afca3c93075c32b965
Na Lin <lna483018@gmail.com>
2022-10-14 17:33:16 -0400

    complete read and write file

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,10 +1,25 @@
 const fs = require("fs");
 
-// todo 1: read context of file
+// TODO 1: File module
+// todo : read and write file synchronous way
+
 const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(textIn);
 
-// todo 2: write context to a file
 const textOutput =
   "this content is written by javascript code: hello world! 😄";
 fs.writeFileSync("./txt/output.txt", textOutput);
+
+// todo: read and write file asynchronous way
+
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  //   console.error(err);
+  //   console.log(err, data);
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    fs.writeFile("./txt/final.txt", `${data1}\n${data2}`, "utf-8", (err) => {
+      if (err) console.log("fail to write");
+      console.log("writed");
+    });
+  });
+});
+console.log("start to reading content from file");
