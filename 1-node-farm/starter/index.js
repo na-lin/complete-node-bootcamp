9d5b114688c6410e2b076b196c49529b7e00478e
Na Lin <lna483018@gmail.com>
2022-10-15 14:16:57 -0400

    build and fill HTML template

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -30,17 +30,57 @@ const url = require("url");
 const apiData = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const productData = JSON.parse(apiData);
 
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+const replacePlaceHolder = function (temp, el) {
+  let output = temp.replace(/{%ID%}/g, el.id);
+  output = output.replace(/{%PRODUCTNAME%}/g, el.productName);
+  output = output.replace(/{%IMAGE%}/g, el.image);
+  output = output.replace(/{%FROM%}/g, el.from);
+  output = output.replace(/{%NUTRIENTS%}/g, el.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, el.quantity);
+  output = output.replace(/{%PRICE%}/g, el.price);
+  output = output.replace(/{%DESCRIPTION%}/g, el.description);
+
+  if (!el.organic) output = output.replace(/{%NON-ORGANIC%}/g, "not-organic");
+  return output;
+};
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
+  //   overview by adding template to dynamic load json data
   if (pathName === "/" || pathName === "/overview") {
-    res.end("This is overview from server");
+    const cards = productData
+      .map((el) => replacePlaceHolder(tempCard, el))
+      .join("");
+    const overviewHTML = tempOverview.replace(/{%PRODUCT-CARDS%}/, cards);
+
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end(overviewHTML);
+
+    // product
   } else if (pathName === "/product") {
     res.end("This is product page from server");
+
+    // api
   } else if (pathName === "/api") {
     res.writeHead(200, {
       "Content-text": "application/json",
     });
     res.end(apiData);
+
+    // Page not found
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
