ea92b9174de2c95205ec383f27d207b1b9a51e30
Na Lin <lna483018@gmail.com>
2022-10-20 17:44:11 -0400

    async/await

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -1,6 +1,7 @@
 const { rejects } = require("assert");
 const fs = require("fs");
 const { resolve } = require("path");
+const { get } = require("superagent");
 const superagent = require("superagent");
 // todo 1: callback hell about http request
 // fs.readFile(`${__dirname}/dog.txt`, "utf-8", (err, data) => {
@@ -51,7 +52,7 @@ const readFilePromise = (filePath) => {
     });
   });
 };
-// get rid of callback function and less error catch
+//// get rid of callback function and less error catch
 const writeFilePromise = (filePath, data) => {
   return new Promise((resolve, reject) => {
     fs.writeFile(filePath, data, (err) => {
@@ -60,14 +61,55 @@ const writeFilePromise = (filePath, data) => {
     });
   });
 };
-readFilePromise(`${__dirname}/dog.txt`)
-  .then((data) => {
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-  })
-  .then((res) => {
-    return writeFilePromise("dog-img.txt", res.body.message);
-  })
-  .then(() => console.log("Rondom dog image saved to file"))
-  .catch((err) => {
+// readFilePromise(`${__dirname}/dog.txt`)
+//   .then((data) => {
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   .then((res) => {
+//     return writeFilePromise("dog-img.txt", res.body.message);
+//   })
+//   .then(() => console.log("Rondom dog image saved to file"))
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// todo 4 : consume promise by async/await
+
+const getDog = async () => {
+  try {
+    const data = await readFilePromise(`${__dirname}/dog.txt`);
+    console.log(data);
+
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    console.log(res.body.message);
+
+    await writeFilePromise("dog-img.txt", res.body.message);
+    console.log("Rondom dog image saved to file");
+    return "2.ready";
+  } catch (err) {
     console.log(err);
-  });
+  }
+};
+
+// return value from async function
+
+// console.log("1. start");
+// const x = getDog();
+// console.log(x);
+// console.log("2. end");
+
+// consume promise by then
+// console.log("1. start");
+// getDog().then((x) => console.log(x));
+// // console.log(x);
+// console.log("3. end");
+
+// don't mix it
+(async function () {
+  console.log("1. start");
+  const x = await getDog();
+  console.log(x);
+  console.log("3. end");
+})();
